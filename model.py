be5db832a57d9e50dd3fc97d3bc2bffb79680bc8
AnandGoud <puli.anand.goud@gmail.com>
2018-07-28 00:11:15 +0530

    added first class

---
+++ model.py
@@ -0,0 +1,14 @@
+# this file contains sample python code
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 27 23:38:00 2018
+
+@author: Anand
+"""
+
+# creation of class 
+class Bicycle:
+    def __init__(self):
+        self.num_whell = 2
+        self.handlebar = "straignt"
+        print("bycle is created")
